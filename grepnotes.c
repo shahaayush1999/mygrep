226b5b92efbc57c05b5a7392a6ea5f60d4662521
Aayush Shah <shahaayush1999@gmail.com>
2018-10-12 13:56:51 +0530

    Added getopt functionality, encapsuled flags and typedefs

--- grepnotes.c
+++ grepnotes.c
@@ -3,7 +3,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 GREP PROBLEM STATEMENT
 Implement these options:  -r  -i  -v  -f  -w  -c  -m  -b -q  -H  -h  -e patterns 
-Also Need to Submit: A file with tested commands - one per linne. File named as test.txt
+Also Need to Submit: A file with tested commands - one per line. File named as test.txt
 
 On Writing a test-suite
 * Some projects (FILE * library, malloc and free library, etc. ) require writing a test suite. It is mentioned against those projects which require a test-suite. Typically it is required for libraries. 
@@ -11,39 +11,17 @@ On Writing a test-suite
 * You are supposed to write an extensive test-suite for testin your code. 
 * You will also be judged on the quality of the test-suite. 
 
-
 Tips to those implementing unix commands
 * Useful C library functions:  getopt(for handling command line options), opendir(opening a directory),  readdir(reading a directory), stat(), chmod(), chown(),  mkdir(),  tmpnam(), tmpfile()
 * Pay special attention to design of a data structure for storing the data. For example n-way tree, stack, stack of strings, array of strings, etc. 
 * Pay special attention to design of how to combine various options together. Normally if you don't do this in the begining, your code will become very complex.
 
-Projects Tasks and Submission Process
-
-Given below is a detailed description of expectations from various projects, and the submission process: 
-
-
-Submission Process:
-a) create your own account on http://gitlab.com  or http://git.fosscommunity.in  (henceforth called 'server') 
-b) create a private (IMP, private!) repository on the server. Give a good name to the repo
-c) Create a README.md file in the repo describing your project
-d) Add the user abhijit13 with permissions as 'master' to your project
-d) Submit the git clone link (with the ".git" in the end) on moodle. E.g. the link looks like this:
-https://gitlab.com/abhijit13/taasika.git
-e) create your own pair of ssh-keys usinng ssh-keygen. Submit your own public key on server to enable write access for yourself.  
-READ: 
-https://docs.gitlab.com/ee/gitlab-basics/create-your-ssh-keys.html
-
-
-Intermediate and continuous evaluation:
-* Learn git on your own. Minimum following commands need to be learnt: 
-  init, add, commit, push, pull, rm, branch, checkout
-* Keep pushing your code to the server every 2-3 days. 
-  Abhijit will keep 'pull'ing your code and checking the   'master' branch.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-Sample grep code picked from
+Sample grep code picked from http://csourcecodes.blogspot.com/2015/03/c-program-to-simulate-grep-command-in.html
+
 #include<stdio.h>
 #include<string.h>
 void main() {
@@ -62,4 +40,63 @@ void main() {
 	}
 	fclose(fp);
 }
-////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+DETAILS OF OPTIONS IN GREP
+
+-r, --recursive
+              Read all files under each directory, recursively, following symbolic links only if they are on the command line.  Note that if no file operand  is  given,  grep  searches  the  working
+              directory.  This is equivalent to the -d recurse option.
+
+/*
+-d ACTION, --directories=ACTION
+              If an input file is a directory, use ACTION to process it.  By default, ACTION is read, i.e., read directories just as if they were ordinary files.  If ACTION is  skip,  silently  skip
+              directories.   If  ACTION  is  recurse,  read  all files under each directory, recursively, following symbolic links only if they are on the command line.  This is equivalent to the -r
+              option.
+*/
+
+-i, --ignore-case
+              Ignore case distinctions in both the PATTERN and the input files.
+
+-v, --invert-match
+              Invert the sense of matching, to select non-matching lines.
+
+-f FILE, --file=FILE
+              Obtain patterns from FILE, one per line.  If this option is used multiple times or is combined with the -e (--regexp) option, search for all patterns given.  The  empty  file  contains
+              zero patterns, and therefore matches nothing.
+
+-w, --word-regexp
+              Select  only  those  lines  containing  matches  that  form whole words.  The test is that the matching substring must either be at the beginning of the line, or preceded by a non-word
+              constituent character.  Similarly, it must be either at the end of the line or followed by a non-word constituent character.  Word-constituent characters are letters, digits,  and  the
+              underscore.
+
+ -c, --count
+              Suppress normal output; instead print a count of matching lines for each input file.  With the -v, --invert-match option (see below), count non-matching lines.
+
+-m NUM, --max-count=NUM
+              Stop reading a file after NUM matching lines.  If the input is standard input from a regular file, and NUM matching lines are output, grep ensures that the standard input is positioned
+              to just after the last matching line before exiting, regardless of the presence of trailing context lines.  This enables a calling process to resume a search.  When  grep  stops  after
+              NUM  matching  lines,  it outputs any trailing context lines.  When the -c or --count option is also used, grep does not output a count greater than NUM.  When the -v or --invert-match
+              option is also used, grep stops after outputting NUM non-matching lines.
+
+-b, --byte-offset
+              Print the 0-based byte offset within the input file before each line of output.  If -o (--only-matching) is specified, print the offset of the matching part itself.
+
+-q, --quiet, --silent
+              Quiet; do not write anything to standard output.  Exit immediately with zero status if any match is found, even if an error was detected.  Also see the -s or --no-messages option.
+
+-H, --with-filename
+              Print the file name for each match.  This is the default when there is more than one file to search.
+
+-h, --no-filename
+              Suppress the prefixing of file names on output.  This is the default when there is only one file (or only standard input) to search.
+
+
+-e PATTERN, --regexp=PATTERN
+              Use  PATTERN  as  the  pattern.   If this option is used multiple times or is combined with the -f (--file) option, search for all patterns given.  This option can be used to protect a
+              pattern beginning with “-”.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- main.c
+++ main.c
@@ -1,7 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef int returnvalue;
+
+typedef struct flags {
+	flag recursive = 0;// -r search through all directories
+	flag ignorecase = 0;// -i ignore upper/lower case
+	flag invertmatch = 0;// -v print all non-matching lines
+	flag selectfile = 0;// -f FILE open this FILE and find string
+	flag wholewords = 0;// -w lines with whole words (letters, digits and underscores NOT okay)
+	flag countdontprint = 0;// -c suppress line print, only print count of matches
+	flag nummatches = 0;// -m NUM stop reading a file after NUM matches, GET MORE INFO HERE!
+	flag byteoffset = 0;// -b print the 0 based byte offset before each line of output
+	flag quiet = 0;// -q dont print anything, exit immediately even if match or error
+	flag withfilename = 0;// -H print filename before each match, default on if more than one file to be scanned
+	flag nofilename = 0;// -h dont print filename, default if only one file to be scanned
+	flag pattern = 0;// -e PATTERN use PATTERN as pattern
+}
+
 
 int main(int argc, char *argv[]) {
+	//declarations
+	
+	returnvalue opt = 0;
+	//fetch options
+	while ((opt = getopt(argc, argv, "rivf:wcm:bqHhe:")) != -1) {
+		switch (opt) {
+			case 'r':
+				break;
+			case 'i':
+				break;
+			case 'v':
+				break;
+			case 'f':
+				break;
+			case 'w':
+				break;
+			case 'c':
+				break;
+			case 'm':
+				break;
+			case 'b':
+				break;
+			case 'q':
+				break;
+			case 'H':
+				break;
+			case 'i':
+				break;
+			case 'e':
+				break;
+			default: /* '?' */
+				fprintf(stderr, "Check Usage in documentation\n");
+				exit(EXIT_FAILURE);
+		}
+	}
 	return 0;
 }
